2529e3b207029e148e0d1ee80187da3d27d960cc
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-01 20:03:50 +0100

    Implementation for installing Magican

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install Magican to /Applications 
+#
+# Examples
+#
+#  include magican
+class magican {
+  package { 'Magican.pkg':
+    provider => 'pkgdmg',
+    source   => 'http://www.magicansoft.com/download/Magican.pkg',
+  }
+}
